f29983f39457ac8d5705185461fb4e2332e44cf9
MSharib <msharibahmed@gmail.com>
2020-02-21 20:49:22 +0530

    Adding new activities

---
+++ app/src/main/java/com/example/android/zfr/Department.java
@@ -0,0 +1,35 @@
+package com.example.android.zfr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class Department extends AppCompatActivity {
+    ListView listView;
+    String[] mItem = {"item1", "item2", "item3", "item3", "item4", "item5"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_department);
+        listView = findViewById(R.id.listView);
+        //Creating ADAPTER class
+
+        MainAdapter adapter = new MainAdapter(this, mItem);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(Department.this,Main2Activity.class);
+                intent.putExtra("word",mItem[position]);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
