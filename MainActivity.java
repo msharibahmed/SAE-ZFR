ac5f6eec6a5f4efd8f9d81baeff35f2a761d830b
msharibahmed <39681411+msharibahmed@users.noreply.github.com>
2020-02-21 19:05:38 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.sae1;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String[] mItem = {"item1", "item2", "item3", "item3", "item4", "item5"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        //Creating ADAPTER class
+
+        MainAdapter adapter = new MainAdapter(this, mItem);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("word",mItem[position]);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+
+
+
+
+}
